78c2f434e5e0a1f9602adde9300a9789d93499c2
Ren-Yaqian <74707036+Ren-Yaqian@users.noreply.github.com>
2021-05-25 15:34:19 +0800

    Add files via upload

---
+++ test0521/Main1.java
@@ -0,0 +1,27 @@
+package test0521;
+
+import java.util.*;
+
+public class Main1{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+            char[] sw=new char[130];
+            String str1="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            String str2="VWXYZABCDEFGHIJKLMNOPQRSTU";
+            for(int i=0;i<str1.length();i++){
+                sw[str1.charAt(i)]=str2.charAt(i);
+            }
+            String str=sc.nextLine();
+            String result="";
+            for(int i=0;i<str.length();i++){
+                if(str.charAt(i)==' '){
+                    result+=" ";
+                }else{
+                    result+=sw[str.charAt(i)];
+                }
+            }
+            System.out.println(result);
+        }
+    }
+}
